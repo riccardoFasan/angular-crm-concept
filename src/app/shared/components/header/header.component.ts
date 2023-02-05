eacb0676bdea7cb8fc6216b2e4842bf7da329b54
Richard <fasanriccardo21@gmail.com>
2023-02-05 11:21:17 +0100

    feat: toolbar links

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,11 +2,19 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatProgressBarModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatProgressBarModule,
+  ],
   template: `
     <mat-progress-bar
       *ngIf="loading"
@@ -17,13 +25,25 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
       [ngStyle]="{ 'marginTop.px': !loading ? '0' : '-4' }"
       color="primary"
     >
-      <h1 class="mat-h2">Material taskbaord</h1>
+      <mat-toolbar-row>
+        <h1 class="mat-h2">Material taskbaord</h1>
+        <button mat-button color="accent" [routerLink]="['taskboard']">
+          Taskboard
+        </button>
+        <button mat-button color="accent" [routerLink]="['taskboard', 'new']">
+          Create new task
+        </button>
+      </mat-toolbar-row>
     </mat-toolbar>
   `,
   styles: [
     `
+      mat-toolbar-row {
+        gap: 0.5rem;
+      }
+
       h1.mat-h2 {
-        margin: 0;
+        margin: 0 1.5rem 0.125rem 0;
       }
     `,
   ],
